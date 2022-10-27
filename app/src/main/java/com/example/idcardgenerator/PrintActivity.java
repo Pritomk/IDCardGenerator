fe90eaa50459acab22a68baf635edcc9cc1d2a19
Pritom <pritom.gangarampur@gmail.com>
2022-10-28 01:04:56 +0530

    do without permission

--- app/src/main/java/com/example/idcardgenerator/PrintActivity.java
+++ app/src/main/java/com/example/idcardgenerator/PrintActivity.java
@@ -49,19 +49,11 @@ public class PrintActivity extends AppCompatActivity {
 
         assignDetails(details,uri);
 
-
-        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 1);
-        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 1);
-
         Button button = binding.printBtn;
         button.setOnClickListener(view -> {
             LinearLayout layout = binding.idCardLayout;
             layout.setDrawingCacheEnabled(true);
             Bitmap b = layout.getDrawingCache();
-
-//            ImageView img = findViewById(R.id.imageView);
-//            img.setImageBitmap(b);
-//            Glide.with(this).load(uri).into(img);
 
             doPhotoPrint(b);
 
@@ -86,62 +78,5 @@ public class PrintActivity extends AppCompatActivity {
         photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
 
         photoPrinter.printBitmap("droids.jpg - test print", bitmap);
-    }
-
-    public static File bitmapToFile(Context context, Bitmap bitmap, String fileNameToSave) { // File name like "image.png"
-        //create a file to write bitmap data
-        File file = null;
-        try {
-            file = new File(Environment.getExternalStorageDirectory() + File.separator + fileNameToSave);
-            file.createNewFile();
-
-//Convert bitmap to byte array
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos); // YOU can also save it in JPEG
-            byte[] bitmapdata = bos.toByteArray();
-
-//write the bytes in file
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(bitmapdata);
-            fos.flush();
-            fos.close();
-            return file;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return file; // it will return null
-        }
-    }
-
-    // Function to check and request permission.
-    public void checkPermission(String permission, int requestCode) {
-        if (ContextCompat.checkSelfPermission(PrintActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
-
-            // Requesting the permission
-            ActivityCompat.requestPermissions(PrintActivity.this, new String[]{permission}, requestCode);
-        } else {
-            Toast.makeText(PrintActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    // This function is called when the user accepts or decline the permission.
-    // Request Code is used to check which permission called this function.
-    // This request code is provided when the user is prompt for permission.
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode,
-                permissions,
-                grantResults);
-
-        if (requestCode == 1) {
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(PrintActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(PrintActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
